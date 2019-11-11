20dbcbc9ae69e7753dbafd249b19ebbb9ed2c58e
Jasmine <jasminecharles@hotmail.com>
2019-11-11 09:37:17 -0800

    class challenge

--- api/api-router.js
+++ api/api-router.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-
+const bcrypt = require('bcrypt.js')
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
 
@@ -9,5 +9,16 @@ router.use('/users', usersRouter);
 router.get('/', (req, res) => {
   res.json({ api: "It's alive" });
 });
+
+router.post('/hash', (req, res) => {
+const password = req.body.password
+
+const hashedPassword = bcrypt.hashSync(password, 12)
+
+return {
+password: password,
+hashedPassword: hashedPassword
+}
+})
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,17 @@
+// const express = require('express');
+
+// const apiRouter = require('./api-router.js');
+
+// const server = express();
+
+// server.use('/api', apiRouter);
+
+// module.exports = server;
+
 const express = require('express');
-
 const apiRouter = require('./api-router.js');
-
+const configureMiddleware = require('./configure-middleware.js');
 const server = express();
-
+configureMiddleware(server);
 server.use('/api', apiRouter);
-
 module.exports = server;
